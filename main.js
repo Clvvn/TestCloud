8208df241295215a20003154ff075a70b0c2c3a7
Clvvn <lapinvlad64@mail.ru>
2024-06-26 21:52:01 +0400

    Update main.js

--- main.js
+++ main.js
@@ -7,36 +7,29 @@ $(document).ready(function() {
         const name = $('#name').val();
         const comment = $('#comment').val();
 
-        var widget = new cp.CloudPayments({
-            language: "ru-RU"
-        });
+        var widget = new cp.CloudPayments();
 
+        // Запуск виджета для одноразовой оплаты или создания рекуррентной подписки
         widget.pay('auth', { // или 'charge'
             publicId: 'pk_aff17de359b486f45c12b4e4fdab0', // id из личного кабинета
             description: 'Пожертвование в благотворительный фонд "Дети в Лете"', // назначение
-            amount: amount, // сумма
+            amount: parseFloat(amount), // сумма
             currency: 'RUB', // валюта
             accountId: email, // идентификатор плательщика (необязательно)
             skin: "mini", // дизайн виджета (необязательно)
-            autoClose: 3, // время в секундах до авто-закрытия виджета (необязательный)
             data: {
                 phone: phone,
                 name: name,
                 comment: comment
-            },
-            configuration: {
-                common: {
-                    successRedirectUrl: "http://test.test/success", // адрес для перенаправления при успешной оплате
-                    failRedirectUrl: "http://test.test/fail" // адрес для перенаправления при неуспешной оплате
-                }
             }
         }, {
             onSuccess: function(options) { // success
                 if (recurring) {
+                    // Если активирован чекбокс ежемесячного платежа, создаем подписку
                     widget.subscription({
                         publicId: 'pk_aff17de359b486f45c12b4e4fdab0',
                         description: 'Ежемесячное пожертвование в благотворительный фонд "Дети в Лете"',
-                        amount: amount,
+                        amount: parseFloat(amount),
                         currency: 'RUB',
                         accountId: email,
                         email: email,
@@ -48,19 +41,23 @@ $(document).ready(function() {
                             name: name,
                             comment: comment
                         }
+                    }, {
+                        onSuccess: function(subscriptionOptions) {
+                            alert('Подписка успешно создана. Спасибо за ваше ежемесячное пожертвование!');
+                        },
+                        onFail: function(reason, subscriptionOptions) {
+                            alert('Ошибка при создании подписки: ' + reason);
+                        }
                     });
+                } else {
+                    alert('Платеж успешно выполнен. Спасибо!');
                 }
-                alert('Платеж успешно выполнен. Спасибо!');
             },
             onFail: function(reason, options) { // fail
-                if (reason === 'User has cancelled') {
-                    alert('Платеж был отменен пользователем.');
-                } else {
-                    alert('Ошибка при оплате: ' + reason);
-                }
+                alert('Ошибка при оплате: ' + reason);
             },
             onComplete: function(paymentResult, options) { // Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
-                // Например вызов вашей аналитики Facebook Pixel
+                // Например вызов вашей аналитики
             }
         });
     });
