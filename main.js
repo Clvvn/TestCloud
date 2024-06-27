06059bb263ad7a9f67d1fdc869bda3134ed3b1e4
Clvvn <lapinvlad64@mail.ru>
2024-06-27 23:31:12 +0500

    fix: +comment+phone

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
+    // Функция для получения и увеличения invoiceId
+    function getNextInvoiceId() {
+        let currentInvoiceId = localStorage.getItem('currentInvoiceId');
+        if (currentInvoiceId === null) {
+            currentInvoiceId = 1;
+        } else {
+            currentInvoiceId = parseInt(currentInvoiceId) + 1;
+        }
+        localStorage.setItem('currentInvoiceId', currentInvoiceId);
+        return currentInvoiceId.toString();
+    }
+
     $('#donate-button').click(function() {
         const amount = parseFloat($('input[name="amount"]:checked').val());
         const recurring = $('#recurring').is(':checked');
         const email = $('#email').val();
-        const phone = $('#phone').val();
+        const phone = $('#phone').val().replace(/[^0-9]/g, ''); // Удаляем все нецифровые символы
         const name = $('#name').val();
         const comment = $('#comment').val();
+        const invoiceId = getNextInvoiceId();
 
         if (isNaN(amount) || !email || !phone || !name) {
             alert('Пожалуйста, заполните все обязательные поля.');
@@ -13,7 +26,7 @@ $(document).ready(function() {
         }
 
         console.log("Запуск CloudPayments виджета с параметрами: ", {
-            amount, email, phone, name, comment, recurring
+            amount, email, phone, name, comment, recurring, invoiceId
         });
 
         var widget = new cp.CloudPayments();
@@ -32,7 +45,7 @@ $(document).ready(function() {
             ],
             taxationSystem: 0,
             email: email,
-            phone: phone,
+            //phone: phone,
             isBso: false,
             amounts: {
                 electronic: amount,
@@ -42,43 +55,62 @@ $(document).ready(function() {
             }
         };
 
-        var data = {};
-        if (recurring) {
-            data.CloudPayments = {
-                CustomerReceipt: receipt,
-                recurrent: {
-                    interval: 'Month',
-                    period: 1,
-                    customerReceipt: receipt
-                }
-            };
-        } else {
-            data.CloudPayments = {
+        var data = {
+            firstName: name.split(" ")[1] ,
+            middleName: name.split(" ")[2],
+            lastName: name.split(" ")[0],
+            phone,
+            comment,
+            CloudPayments: {
                 CustomerReceipt: receipt
+            }
+        };
+        if (recurring) {
+            data.CloudPayments.recurrent = {
+                interval: 'Month',
+                period: 1,
+                customerReceipt: receipt
             };
         }
 
-        // Добавляем комментарий 
-        if (comment) {
-            data.CloudPayments.CustomerReceipt.Items.push({
-                label: 'Благодарим за Подертвование!: ' ,
-                amount: amount,
-                quantity: 1,
-                price: amount,
-                vat: 0,
-                method: 0,
-                object: 4,
-            });
-        }
+        // Добавляем комментарий в чек, если он указан
+    //    if (comment) {
+    //         receipt.Items.push({
+    //             label: 'Комментарий: ' + comment,
+    //             amount: 0,
+    //             quantity: 1,
+    //             price: 0,
+    //             vat: 0,
+    //             method: 0,
+    //             object: 4,
+    //         });
+    //  }
+
+        // Формируем объект payer
+        var payer = {
+        
+            phone: phone
+        };
 
         widget.charge({
             publicId: 'pk_aff17de359b486f45c12b4e4fdab0',
             description: 'Пожертвование в благотворительный фонд "Дети в Лете"',
             amount: amount,
             currency: 'RUB',
-            invoiceId: '1234567',
             accountId: email,
-            data: data
+            invoiceId: invoiceId,
+            email: email,
+            skin: "mini",
+            autoClose: 3,
+            data: data,
+            configuration: {
+                common: {
+                    successRedirectUrl: "https://your-site.com/success", 
+                    failRedirectUrl: "https://your-site.com/fail" 
+                }
+            },
+            payer: payer 
+          
         },
         function (options) { // success
             console.log('Успешный платеж: ', options);
@@ -99,7 +131,6 @@ $(document).ready(function() {
         function (paymentResult, options) { // onComplete
             console.log('Получен ответ с результатом транзакции: ', paymentResult);
         });
-        
 
     });
 });
